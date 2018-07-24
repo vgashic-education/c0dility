bbf23b6f9bc108973c7e9a948f8063069fb5e261
Vladimir Gasic <vgashic@gmail.com>
2018-07-24 02:32:26 +0200

    Initial commit

---
+++ CyclicRotation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CyclicRotation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = new int[] { 3, 8, 9, 7, 6 };
+
+			solution(arr, 3);
+		}
+
+		public static int[] solution(int[] A, int K)
+		{
+			// write your code in C# 6.0 with .NET 4.5 (Mono)
+			int[] shifted = ShiftArrayRight(A, K);
+
+			return shifted;
+		}
+
+		public static int[] ShiftArrayRight(int[] arrayToShift, int shiftTimes)
+		{
+			IEnumerable<int> result = arrayToShift.ToList();
+			int length = arrayToShift.Length;
+
+			for (int i = 1; i <= shiftTimes; i++)
+			{
+				IEnumerable<int> firstPart = result.Skip(length - 1);
+				IEnumerable<int> secondPart = result.Take(length - 1);
+
+				result = firstPart.Concat(secondPart).ToArray();
+			}
+
+			int[] resultArray = result.ToArray();
+
+			return resultArray;
+		}
+	}
+}
---
+++ OddOccurrencesInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrencesInArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] A = { 9, 3, 9, 3, 9, 7, 9 };
+
+			solution(A);
+		}
+
+
+		public static int solution(int[] A)
+		{
+			List<int> list = A.ToList();
+
+			var grouped = list.GroupBy(a => a);
+
+			int singleElement = grouped.Where(x => x.Count() == 1).FirstOrDefault().Key;
+
+			return singleElement;
+		}
+	}
+}
